2fc708eaf2300964011e7f76703afeebd7e60c64
Nishant Kumar Behl <Nishant.Behl@oneadvanced.com>
2021-09-02 06:59:58 +0530

    MergeSortIntital

--- src/Sort/MergeSort.java
+++ src/Sort/MergeSort.java
@@ -1,4 +1,13 @@
 package Sort;
+import java.util.*;
 
 public class MergeSort {
+
+    //Code the calling function
+
+        public  static void main (String[] args) {
+            
+            
+        }
+
 }
