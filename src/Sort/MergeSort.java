6e8cf891540c5c6719b294b76bf688c8718c627a
Nishant Kumar Behl <Nishant.Behl@oneadvanced.com>
2021-09-02 07:18:08 +0530

    Defined all methods

--- src/Sort/MergeSort.java
+++ src/Sort/MergeSort.java
@@ -6,8 +6,35 @@ public class MergeSort {
     //Code the calling function
 
         public  static void main (String[] args) {
-            
+             int arr[] = {18,5,14,30,9}; 
+             int l =0, r=4;
+
+            mergsort(arr,l,r);
+
+            //print the output
+            for (int x : arr)
+                System.out.println(x + " ");
+
+        }
+
+        static void merge(int arr[], int l, int m, int k){
+
             
         }
+
+        // Define mergsort method\function
+
+        static void mergsort(int a[], int l, int r) {
+                if (r > l){
+                    int m = (r-l)/2;
+                    mergsort(a, l, m);
+                    mergsort(a, m+1, r);
+                    merge(arr,l,m,r)
+
+                }
+
+        }
+
+    
 
 }
