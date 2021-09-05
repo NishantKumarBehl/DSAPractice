1d41916c97fdb029f3770b0fbe0d85c6c644434e
Nishant Kumar Behl <Nishant.Behl@oneadvanced.com>
2021-09-05 06:07:06 +0530

    QuickSort-defined main and quicksort method

--- src/Sort/QuickSort.java
+++ src/Sort/QuickSort.java
@@ -1,4 +1,17 @@
 package Sort;
 
 public class QuickSort {
+
+    
+    public static void main(String[] args){
+        int[] a = {8,13,2,23,19,20,6};
+        int low = 0,high=a.length -1;
+        quicksort(a,low,high);
+        //print the sorted array
+        for (int x : a)
+            System.out.println(x + " ");
+    }
+
+    
+
 }
