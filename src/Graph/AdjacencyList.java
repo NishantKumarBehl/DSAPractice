7ca103c9d562c623518a28bc615eee12ee8dde03
Nishant Kumar Behl <Nishant.Behl@oneadvanced.com>
2021-09-05 16:34:01 +0530

    Added Graph Directory and Adjancency matrix file

---
+++ src/Graph/AdjacencyList.java
@@ -0,0 +1,18 @@
+package Graph;
+
+import java.util.*;
+
+public class AdjacencyList {
+
+	public static void main(String[] args) 
+	{  
+		int V = 4; 
+		ArrayList<ArrayList<Integer> > adj = new ArrayList<ArrayList<Integer> >(V); 
+		
+		for (int i = 0; i < V; i++) 
+			adj.add(new ArrayList<Integer>()); 
+
+
+		
+    }
+}
